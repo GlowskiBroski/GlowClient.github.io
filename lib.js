0c91affe70d27eb97c3191549790c79345f66ec2
GlowClient <35705519+GlowClient@users.noreply.github.com>
2018-05-30 16:45:14 -0400

    Delete lib.js

--- lib.js
+++
@@ -1,9 +0,0 @@
-function toggleComponent(toBeToggled, toggler, ms) {
-    if($(toBeToggled).is(':visible')) {
-        $(toggler).html('(+)');
-        $(toBeToggled).hide(ms);
-    } else {
-        $(toggler).html('(-)');
-        $(toBeToggled).show(ms);
-    }
-}
